d3dc64d88c4fc1287cb5c2f69d59e150552cef1d
Ilya Prokhorov <79151474620@yandex.ru>
2021-07-07 14:18:38 +0300

    pom.xml etc edit

---
+++ plugin-adls/src/main/java/JettyRangerClient.java
@@ -0,0 +1,18 @@
+/*
+ *
+ * JettyRangerClient
+ *
+ */
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JettyRangerClient {
+    private static final Logger LOG = LoggerFactory.getLogger(JettyRangerClient.class);
+
+    public static void main(String[] args) {
+        LOG.info("==> [start] JettyRangerClient");
+
+        LOG.info("<== [stop] JettyRangerClient");
+    }
+}
