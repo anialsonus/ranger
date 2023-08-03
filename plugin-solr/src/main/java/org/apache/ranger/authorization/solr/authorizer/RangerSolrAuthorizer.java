2568587ae52f7371f9d2cacb5f141bad30190f38
Andrey Shitov <sai@arenadata.io>
2023-08-03 13:48:44 +0500

    Fixed solr deps.

--- plugin-solr/src/main/java/org/apache/ranger/authorization/solr/authorizer/RangerSolrAuthorizer.java
+++ plugin-solr/src/main/java/org/apache/ranger/authorization/solr/authorizer/RangerSolrAuthorizer.java
@@ -428,7 +428,7 @@ public class RangerSolrAuthorizer extends SearchComponent implements Authorizati
 		if (httpServletRequest == null) {
 			SolrCore solrCore = rb.req.getCore();
 			StringBuilder builder = new StringBuilder("Unable to locate HttpServletRequest");
-			if (solrCore != null && !solrCore.getSolrConfig().get("requestDispatcher/requestParsers/@addHttpRequestToContext").boolAttr(null, true)) {
+			if (solrCore != null && !solrCore.getSolrConfig().get("requestDispatcher/requestParsers/@addHttpRequestToContext").boolVal(true)) {
 				builder.append(", ensure requestDispatcher/requestParsers/@addHttpRequestToContext is set to true in solrconfig.xml");
 			}
 			throw new SolrException(SolrException.ErrorCode.UNAUTHORIZED, builder.toString());
@@ -732,7 +732,7 @@ public class RangerSolrAuthorizer extends SearchComponent implements Authorizati
 		HttpServletRequest httpServletRequest = (HttpServletRequest) req.getContext().get("httpRequest");
 		if (httpServletRequest == null) {
 			StringBuilder builder = new StringBuilder("Unable to locate HttpServletRequest");
-			if (solrCore != null && !solrCore.getSolrConfig().getBool("requestDispatcher/requestParsers/@addHttpRequestToContext", true)) {
+			if (solrCore != null && !solrCore.getSolrConfig().get("requestDispatcher/requestParsers/@addHttpRequestToContext").boolVal(true)) {
 				builder.append(", ensure requestDispatcher/requestParsers/@addHttpRequestToContext is set to true in solrconfig.xml");
 			}
 			throw new SolrException(SolrException.ErrorCode.UNAUTHORIZED, builder.toString());
