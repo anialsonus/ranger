2917f7a0edcbe98edeb36760d958731f0a3d700f
Andrey Shitov <sai@arenadata.io>
2023-10-05 15:14:26 +0500

    RANGER-3914 backport

--- security-admin/db/sqlserver/optimized/current/ranger_core_db_sqlserver.sql
+++ security-admin/db/sqlserver/optimized/current/ranger_core_db_sqlserver.sql
@@ -964,9 +964,9 @@ CREATE TABLE [dbo].[x_portal_user](
         [email] [varchar](512) DEFAULT NULL NULL,
         [status] [int] DEFAULT 0 NOT NULL,
         [user_src] [int] DEFAULT 0 NOT NULL,
-        [notes] [varchar](4000) DEFAULT NULL NULL,
-        [other_attributes] [varchar](4000) DEFAULT NULL NULL,
-        [sync_source] [varchar](4000) DEFAULT NULL NULL,
+        [notes] [nvarchar](max) DEFAULT NULL NULL,
+        [other_attributes] [nvarchar](max) DEFAULT NULL NULL,
+        [sync_source] [nvarchar](max) DEFAULT NULL NULL,
 PRIMARY KEY CLUSTERED
 (
         [id] ASC
@@ -1112,14 +1112,14 @@ CREATE TABLE [dbo].[x_group](
         [added_by_id] [bigint] DEFAULT NULL NULL,
         [upd_by_id] [bigint] DEFAULT NULL NULL,
         [group_name] [nvarchar](767) NOT NULL,
-        [descr] [nvarchar](4000) NOT NULL,
+        [descr] [nvarchar](max) DEFAULT NULL NULL,
         [status] [int] DEFAULT 0  NOT NULL,
         [group_type] [int] DEFAULT 0 NOT NULL,
         [cred_store_id] [bigint] DEFAULT NULL NULL,
         [group_src] [int] DEFAULT 0 NOT NULL,
         [is_visible] [int] DEFAULT 1 NOT NULL,
-        [other_attributes] [varchar](4000) DEFAULT NULL NULL,
-        [sync_source] [varchar](4000) DEFAULT NULL NULL,
+        [other_attributes] [nvarchar](max) DEFAULT NULL NULL,
+        [sync_source] [nvarchar](max) DEFAULT NULL NULL,
 PRIMARY KEY CLUSTERED
 (
         [id] ASC
@@ -1158,12 +1158,12 @@ CREATE TABLE [dbo].[x_user](
         [added_by_id] [bigint] DEFAULT NULL NULL,
         [upd_by_id] [bigint] DEFAULT NULL NULL,
         [user_name] [nvarchar](767) NOT NULL,
-        [descr] [nvarchar](4000) NOT NULL,
+        [descr] [nvarchar](max) DEFAULT NULL NULL,
         [status] [int] DEFAULT 0 NOT NULL,
         [cred_store_id] [bigint] DEFAULT NULL NULL,
         [is_visible] [int] DEFAULT 1 NOT NULL,
-        [other_attributes] [varchar](4000) DEFAULT NULL NULL,
-        [sync_source] [varchar](4000) DEFAULT NULL NULL,
+        [other_attributes] [nvarchar](max) DEFAULT NULL NULL,
+        [sync_source] [nvarchar](max) DEFAULT NULL NULL,
 PRIMARY KEY CLUSTERED
 (
         [id] ASC
@@ -4101,6 +4101,7 @@ INSERT INTO x_db_version_h (version,inst_at,inst_by,updated_at,updated_by,active
 INSERT INTO x_db_version_h (version,inst_at,inst_by,updated_at,updated_by,active) VALUES ('054',CURRENT_TIMESTAMP,'Ranger 1.0.0',CURRENT_TIMESTAMP,'localhost','Y');
 INSERT INTO x_db_version_h (version,inst_at,inst_by,updated_at,updated_by,active) VALUES ('055',CURRENT_TIMESTAMP,'Ranger 1.0.0',CURRENT_TIMESTAMP,'localhost','Y');
 INSERT INTO x_db_version_h (version,inst_at,inst_by,updated_at,updated_by,active) VALUES ('056',CURRENT_TIMESTAMP,'Ranger 1.0.0',CURRENT_TIMESTAMP,'localhost','Y');
+INSERT INTO x_db_version_h (version,inst_at,inst_by,updated_at,updated_by,active) VALUES ('060',CURRENT_TIMESTAMP,'Ranger 1.0.0',CURRENT_TIMESTAMP,'localhost','Y');
 INSERT INTO x_db_version_h (version,inst_at,inst_by,updated_at,updated_by,active) VALUES ('DB_PATCHES',CURRENT_TIMESTAMP,'Ranger 1.0.0',CURRENT_TIMESTAMP,'localhost','Y');
 INSERT INTO x_user_module_perm (user_id,module_id,create_time,update_time,added_by_id,upd_by_id,is_allowed) VALUES (dbo.getXportalUIdByLoginId('admin'),dbo.getModulesIdByName('Reports'),CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,dbo.getXportalUIdByLoginId('admin'),dbo.getXportalUIdByLoginId('admin'),1);
 INSERT INTO x_user_module_perm (user_id,module_id,create_time,update_time,added_by_id,upd_by_id,is_allowed) VALUES (dbo.getXportalUIdByLoginId('admin'),dbo.getModulesIdByName('Resource Based Policies'),CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,dbo.getXportalUIdByLoginId('admin'),dbo.getXportalUIdByLoginId('admin'),1);
